672bb5807f33b57beeee1c8c81fe50141f39acee
StefanosKok <42679424+StefanosKok@users.noreply.github.com>
2020-06-18 19:56:26 +0300

    build

---
+++ build/precache-manifest.6a9820094d582d6963a6193ffee278aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b300f56c3d38282042c4f592a82d1b",
+    "url": "/COVID-19TrackerAPP/index.html"
+  },
+  {
+    "revision": "1a36fde2d852b2c114ca",
+    "url": "/COVID-19TrackerAPP/static/css/main.c22e0bd1.chunk.css"
+  },
+  {
+    "revision": "5e9ddf9f9fd5b136dd74",
+    "url": "/COVID-19TrackerAPP/static/js/2.ffb43513.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19TrackerAPP/static/js/2.ffb43513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a36fde2d852b2c114ca",
+    "url": "/COVID-19TrackerAPP/static/js/main.cd10b8c6.chunk.js"
+  },
+  {
+    "revision": "32d34e0b4cb1c60cb024",
+    "url": "/COVID-19TrackerAPP/static/js/runtime-main.08bfe3f7.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19TrackerAPP/static/media/image.d7265326.png"
+  }
+]);
