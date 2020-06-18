611734acea14e97e29e2dd18afc622b3e87b5639
StefanosKok <42679424+StefanosKok@users.noreply.github.com>
2020-06-18 19:48:31 +0300

    init

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+//This file takes all the components and exports them in app.js
+export { default as Cards } from './Cards/Cards';
+export { default as Chart } from './Chart/Chart';
+export { default as CountryPicker } from './CountryPicker/CountryPicker';
